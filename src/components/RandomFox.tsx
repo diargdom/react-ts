c5429d2b65999e826ffc54c7f1c113b43c073fdb
Diego Argumedo <diegoargumedo.jcol@orange.com>
2025-02-10 11:09:12 -0500

    Feat: State con tipos primitivos

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,4 +1,9 @@
-type Props = { image: string };
-export const RandomFox = ({ image }: Props): JSX.Element => {
-  return <img width={320} height="auto" src={image} className="rounded" />;
+import { JSX } from "react";
+
+type Props = { image: string; alt: string };
+export const RandomFox = ({ image, alt }: Props): JSX.Element => {
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img width={320} height="auto" src={image} className="rounded" alt={alt} />
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import { RandomFox } from "@/components/RandomFox";
+import { useState } from "react";
 
 //generate a random function between 1 and 123
 const random = (): number => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
   return (
     <div>
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
+        {images.map((image, index) => (
+          <div key={index} className="p-4">
+            <RandomFox image={image} alt="Imagenes random de zorros" />
+          </div>
+        ))}
       </main>
       <footer></footer>
     </div>
