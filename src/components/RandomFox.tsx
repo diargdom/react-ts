7d10e4e99d271206ed0124f07a1070778d0a3cd5
Diego Argumedo <diegoargumedo.jcol@orange.com>
2025-02-10 10:38:58 -0500

    Feat: Objeto prop y children

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,38 +1,4 @@
-// Forma explícita "JSX.Element"** → Es la recomendada
-//generate a random function between 1 and 123
-const random = () => Math.floor(Math.random() * 123) + 1;
-
-export const RandomFox = (): JSX.Element => {
-  const image = `https://randomfox.ca/images/${random()}.jpg`;
+type Props = { image: string };
+export const RandomFox = ({ image }: Props): JSX.Element => {
   return <img width={320} height="auto" src={image} className="rounded" />;
 };
-
-// // Usando tipos de React "FunctionComponent"**
-// // Forma implícito (JSX:Element)
-// export const RandomFox = () => {
-//   return (
-//     <div>
-//       <h2>Hola!</h2>
-//     </div>
-//   );
-// };
-// import type { FunctionComponente } from "react";
-
-// export const RandomFox: FunctionComponent = () => {
-//   return (
-//     <div>
-//       <h2>Hola!</h2>
-//     </div>
-//   );
-// };
-
-// // Usando tipos de React "FC"**
-// import type { FC } from "react";
-
-// export const RandomFox: FC = () => {
-//   return (
-//     <div>
-//       <h2>Hola!</h2>
-//     </div>
-//   );
-// };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { RandomFox } from "@/components/RandomFox";
 
+//generate a random function between 1 and 123
+const random = (): number => Math.floor(Math.random() * 123) + 1;
+
 export default function Home() {
   return (
     <div>
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <RandomFox />
+        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
       </main>
       <footer></footer>
     </div>
